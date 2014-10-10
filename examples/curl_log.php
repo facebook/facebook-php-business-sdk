1b0525f8531dc60103a9b89df5a4fa9d9939861c
Paul Bain <paulbain@fb.com>
2014-10-10 03:57:01 -0700

    Adds CurlLogger to output curl commands from SDK requests
    Summary: Using the CurlLogger will enable developers to easily see the calls being made in standard output, and can be used when reporting bugs.
    
    Test Plan: Use the curl_log.php in examples, or a fun one is to change the logger used by tests and run!

---
+++ examples/curl_log.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * Copyright 2014 Facebook, Inc.
+ *
+ * You are hereby granted a non-exclusive, worldwide, royalty-free license to
+ * use, copy, modify, and distribute this software in source code or binary
+ * form for use in connection with the web services and APIs provided by
+ * Facebook.
+ *
+ * As with any software that integrates with the Facebook platform, your use
+ * of this software is subject to the Facebook Developer Principles and
+ * Policies [http://developers.facebook.com/policy/]. This copyright notice
+ * shall be included in all copies or substantial portions of the software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ *
+ */
+
+// Set your access token here:
+$access_token = null;
+$app_id = null;
+$app_secret = null;
+$account_id = null;
+
+if (is_null($access_token) || is_null($app_id) || is_null($app_secret)) {
+  throw new \Exception(
+    'You must set your access token, app id and app secret before executing'
+  );
+}
+
+if (is_null($account_id)) {
+  throw new \Exception(
+    'You must set your account id before executing');
+}
+
+define('SDK_DIR', __DIR__ . '/..'); // Path to the SDK directory
+$loader = include SDK_DIR.'/vendor/autoload.php';
+
+use FacebookAds\Api;
+use Facebook\FacebookSession;
+use FacebookAds\CurlLogger;
+
+FacebookSession::setDefaultApplication($app_id, $app_secret);
+$session = new FacebookSession($access_token);
+
+// Create the CurlLogger
+$logger = new CurlLogger();
+
+// To write to a file pass in a file handler
+// $logger = new CurlLogger(fopen('test','w'));
+
+// If you need to escape double quotes, use the following - useful for docs
+$logger->setEscapeQuotes(true);
+
+// Create the Api instance
+$api = new Api($session, $logger);
+
+
+use FacebookAds\Object\AdAccount;
+use FacebookAds\Object\Fields\AdAccountFields;
+
+$account = (new AdAccount($account_id))->read(array(
+  AdAccountFields::ID,
+  AdAccountFields::NAME,
+  AdAccountFields::ACCOUNT_STATUS
+));
