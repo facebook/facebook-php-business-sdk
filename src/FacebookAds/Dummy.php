8306fe880bdd65a2c979789d16fad13992886fe5
Luca Bruno <pruno@fb.com>
2015-02-11 01:45:00 +0000

    Add line of more than 80 chars

--- src/FacebookAds/Dummy.php
+++ src/FacebookAds/Dummy.php
@@ -5,7 +5,7 @@ class wrong_coding_style_pr
 
 public function a()
 {
-  
+  return "this line will easily break the 80 chars limit if i continue to write useless things in this string"; 
 }
 
   static protected function b($x) {
