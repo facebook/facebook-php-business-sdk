e8f968972f3970482519040cf4901a4dddc027fb
Le Tuan Minh <minh_lt@septeni-technology.jp>
2015-03-19 22:29:33 +0700

    add is_autobid

--- src/FacebookAds/Object/Fields/AdSetFields.php
+++ src/FacebookAds/Object/Fields/AdSetFields.php
@@ -45,4 +45,6 @@ abstract class AdSetFields {
   const UPDATED_TIME = 'updated_time';
   const TARGETING = 'targeting';
   const PROMOTED_OBJECT = 'promoted_object';
+  const IS_AUTOBIS = 'is_autobid';	
+	
 }
