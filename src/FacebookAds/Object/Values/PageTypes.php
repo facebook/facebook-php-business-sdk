fb954d9227838e077e15019d17a115e1ab1016e6
Dulio Matos Leite <duliomatos@fb.com>
2016-04-14 07:40:22 -0700

    Add instagramstream to PageTypes
    Summary:
    This value is missing in autogen.
    Was fixed in manual sdk coming from pull request #198.
    
    Test Plan: NA

--- src/FacebookAds/Object/Values/PageTypes.php
+++ src/FacebookAds/Object/Values/PageTypes.php
@@ -33,6 +33,7 @@ class PageTypes extends AbstractEnum {
 
   const DESKTOP_FEED = 'desktopfeed';
   const HOME = 'home';
+  const INSTAGRAM_STREAM = 'instagramstream';
   const LEGACY_EXTERNAL = 'legacyexternal';
   const LOGOUT = 'logout';
   const MOBILE_EXTERNAL = 'mobileexternal';
