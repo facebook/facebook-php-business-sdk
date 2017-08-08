5d9045f8ddad3988f4e185ebd4104968fd2021f1
Ryan Walsh <rcwalsh@gmail.com>
2017-08-08 16:04:03 -0400

    Fixes the bug where the LOOKALIKE_VALUE schema field was getting hashed even when $is_hashed = false in the addUsers method of CustomAudienceMultiKey
    See https://developers.facebook.com/bugs/565178463606247 for the original report and steps to reproduce.

--- src/FacebookAds/Object/CustomAudienceNormalizers/HashNormalizer.php
+++ src/FacebookAds/Object/CustomAudienceNormalizers/HashNormalizer.php
@@ -26,29 +26,30 @@ namespace FacebookAds\Object\CustomAudienceNormalizers;
 
 use FacebookAds\Object\CustomAudienceMultiKey;
 use FacebookAds\Object\CustomAudienceNormalizers\ValueNormalizerInterface;
+use FacebookAds\Object\Fields\CustomAudienceMultikeySchemaFields;
 
 class HashNormalizer implements ValueNormalizerInterface {
 
-  /**
-   *  @var string
-   */
-  const HASH_TYPE_SHA256 = 'sha256';
+    /**
+     *  @var string
+     */
+    const HASH_TYPE_SHA256 = 'sha256';
 
-  /**
-   * @param string $key
-   * @param string $key_value
-   * @return boolean
-   */
-  public function shouldNormalize($key, $key_value) {
-    return true;
-  }
+    /**
+     * @param string $key
+     * @param string $key_value
+     * @return boolean
+     */
+    public function shouldNormalize($key, $key_value) {
+        return $key !== CustomAudienceMultikeySchemaFields::LOOKALIKE_VALUE;
+    }
 
-  /**
-   * @param string $key
-   * @param string $key_value
-   * @return string
-   */
-  public function normalize($key, $key_value) {
-    return hash(self::HASH_TYPE_SHA256, $key_value);
-  }
+    /**
+     * @param string $key
+     * @param string $key_value
+     * @return string
+     */
+    public function normalize($key, $key_value) {
+        return hash(self::HASH_TYPE_SHA256, $key_value);
+    }
 }
