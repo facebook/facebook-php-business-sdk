4ba67c0edb0d41692aab18625ede16418b7c29a2
Jing Ping <jingping@fb.com>
2019-09-25 22:32:03 -0700

    Update tag when new version release in Travis CI

---
+++ .travis/tag_version.sh
@@ -0,0 +1,15 @@
+VERSION_CLASS=src/FacebookAds/ApiConfig.php
+
+VERSION=$(sed -n "s/.*SDKVersion = '\(.*\)\';/\1/p" $VERSION_CLASS)
+GIT_RAW_TAG=$(git describe --abbrev=0 --tags)
+
+echo $VERSION
+echo $GIT_RAW_TAG
+if [ "$VERSION" == "$GIT_RAW_TAG" ]; then
+  echo 'versions are updated, no need to add new tag.'
+else
+  echo 'versions are not updated, start to add new tag.'
+  git tag $VERSION -a -m "Version $VERSION" || die "get error when add new tag"
+  echo 'push tag...'
+  git push origin $VERSION
+fi
