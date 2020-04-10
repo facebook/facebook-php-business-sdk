00ee5563b394175bc9152b5a7526fcbb8077290a
David Yeghshatyan <david6@fb.com>
2020-04-10 11:40:58 -0700

    Add new util functions
    Summary: Migrated functions that will be common to integrations from Wordress into BizSDK
    
    Reviewed By: HeyMultiverse
    
    Differential Revision: D20526502
    
    fbshipit-source-id: dbb70da6

--- src/FacebookAds/Object/ServerSide/Util.php
+++ src/FacebookAds/Object/ServerSide/Util.php
@@ -51,4 +51,85 @@ class Util {
     return preg_match('/^[A-Fa-f0-9]{64}$/', $pii) ||
       preg_match('/^[a-f0-9]{32}$/', $pii);
   }
+
+  /**
+   * Extracts the IP Address from the PHP Request Context.
+   * @return string
+   */
+  public static function getIpAddress() {
+    $ip_address = null;
+
+    if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+      $ip_address = $_SERVER['HTTP_CLIENT_IP'];
+    } else if (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+      $ip_address = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    } else if (!empty($_SERVER['REMOTE_ADDR'])) {
+      $ip_address = $_SERVER['REMOTE_ADDR'];
+    }
+
+    return $ip_address;
+  }
+
+  /**
+   * Extracts the HTTP User Agent from the PHP Request Context.
+   * @return string
+   */
+  public static function getHttpUserAgent() {
+    $user_agent = null;
+
+    if (!empty($_SERVER['HTTP_USER_AGENT'])) {
+      $user_agent = $_SERVER['HTTP_USER_AGENT'];
+    }
+
+    return $user_agent;
+  }
+
+  /**
+   * Extracts the URI from the PHP Request Context.
+   * @return string
+   */
+  public static function getRequestUri() {
+    $url = "http://";
+    if (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') {
+      $url = "https://";
+    }
+
+    if (!empty($_SERVER['HTTP_HOST'])) {
+      $url .= $_SERVER['HTTP_HOST'];
+    }
+
+    if (!empty($_SERVER['REQUEST_URI'])) {
+      $url .= $_SERVER['REQUEST_URI'];
+    }
+
+    return $url;
+  }
+
+  /**
+   * Extracts the FBP cookie from the PHP Request Context.
+   * @return string
+   */
+  public static function getFbp() {
+    $fbp = null;
+
+    if (!empty($_COOKIE['_fbp'])) {
+      $fbp = $_COOKIE['_fbp'];
+    }
+
+    return $fbp;
+  }
+
+  /**
+   * Extracts the FBC cookie from the PHP Request Context.
+   * @return string
+   */
+  public static function getFbc() {
+    $fbc = null;
+
+    if (!empty($_COOKIE['_fbc'])) {
+      $fbc = $_COOKIE['_fbc'];
+    }
+
+    return $fbc;
+  }
 }
