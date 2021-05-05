33d966491d84361b90fcc3b521cc954e404d3082
Terry Hunter <terry.hunter@retortal.com>
2021-05-05 09:43:31 +0100

    * Update version config to 9.0.0

--- src/FacebookAds/ApiConfig.php
+++ src/FacebookAds/ApiConfig.php
@@ -23,7 +23,7 @@
  */
 namespace FacebookAds;
 class ApiConfig {
-  const APIVersion = '8.0';
-  const SDKVersion = '8.0.0';
+  const APIVersion = '9.0';
+  const SDKVersion = '9.0.0';
   const TYPE_CHECKER_STRICT_MODE = false;
 }
