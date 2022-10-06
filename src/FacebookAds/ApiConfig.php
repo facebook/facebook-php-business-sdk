f73883c9bba6f558a1476b8f18480af2bbc0b040
Dario La Padula <dario86ostia@gmail.com>
2022-10-06 13:15:58 +0200

    APIVersion and SDKVersion updated to 15.0

--- src/FacebookAds/ApiConfig.php
+++ src/FacebookAds/ApiConfig.php
@@ -23,7 +23,7 @@
  */
 namespace FacebookAds;
 class ApiConfig {
-  const APIVersion = '14.0';
-  const SDKVersion = '14.0.0';
+  const APIVersion = '15.0';
+  const SDKVersion = '15.0.0';
   const TYPE_CHECKER_STRICT_MODE = false;
 }
