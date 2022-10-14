10f9ed3ebe5fe109e6ee4830d790bf40e83eb1d6
Jude Tillekeratne <jtilleke@fb.com>
2022-10-13 21:51:16 -0700

    Adding customEndpointRequest for PHP implementation [1/X]
    Summary:
    Following Design Doc: https://docs.google.com/document/d/1_19vhaezTArpba783Qaxv_h8QjJubBbWX06yJllSdpQ/edit
    The components in this diff are highlighted in red below.
    {F780690217}
    The new interface is called CustomEndpointRequest and the implementation for our case will be CAPIGIngressEndpoint.
    
    Reviewed By: ztlbells
    
    Differential Revision: D40334895
    
    fbshipit-source-id: 2f5d4382574e44c6e7f8457c27f61bb3d5a39d49

---
+++ src/FacebookAds/Object/ServerSide/CustomEndpointRequest.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * Copyright (c) 2015-present, Facebook, Inc. All rights reserved.
+ *
+ * You are hereby granted a non-exclusive, worldwide, royalty-free license to
+ * use, copy, modify, and distribute this software in source code or binary
+ * form for use in connection with the web services and APIs provided by
+ * Facebook.
+ *
+ * As with any software that integrates with the Facebook platform, your use
+ * of this software is subject to the Facebook Developer Principles and
+ * Policies [http://developers.facebook.com/policy/]. This copyright notice
+ * shall be included in all copies or substantial portions of the software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ *
+ */
+
+use FacebookAds\Object\ServerSide\Event;
+use FacebookAds\Object\ServerSide\EventResponse;
+use GuzzleHttp\Promise\PromiseInterface;
+
+/**
+ * Interface for Custom Endpoint Request
+ *
+ * @category Class
+ */
+interface CustomEndpointRequest {
+    /**
+     * Synchronously send events to custom endpoint
+     * @param string $pixel_id
+     * @param array<Event> $events
+     * @return EventResponse
+     */
+    public function sendEvent(string $pixel_id, array $events): EventResponse;
+
+    /**
+     * Asynchronously send events to custom endpoint
+     * @param string $pixel_id
+     * @param array<Event> $events
+     * @return PromiseInterface
+     */
+    public function sendEventAsync(string $pixel_id, array $events): PromiseInterface;
+    /**
+     * Create a filter function that will filter events being sent to endpoint
+     * @param Filter $filter
+     */
+    public function setFilter(Filter $filter);
+}
+interface Filter {
+    /**
+     * Returns a boolean on whether the event should be filtered
+     * @param Event $event
+     * @return bool
+     */
+    public function shouldSendEvent(Event $event):bool;
+    }
+?>
+
