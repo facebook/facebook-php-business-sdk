bfec989895d5df4e89fc3b2dd398b42a83d11a33
Shuotian Cheng <shuotian@meta.com>
2024-01-23 11:52:49 -0800

    Support KeyValue Type for Ruby PHP and Python
    Summary: Add support of type KeyValue to Ruby, PHP, and Python.
    
    Reviewed By: mengxuanzhangz
    
    Differential Revision: D53016094
    
    fbshipit-source-id: 50409b3f7f5f8df38853e266edf46e781c269a47

--- src/FacebookAds/Object/Fields/LeadgenFormFields.php
+++ src/FacebookAds/Object/Fields/LeadgenFormFields.php
@@ -70,7 +70,7 @@ class LeadgenFormFields extends AbstractEnum {
       'questions' => 'list<LeadGenQuestion>',
       'status' => 'string',
       'thank_you_page' => 'LeadGenThankYouPage',
-      'tracking_parameters' => 'list<map<string, string>>',
+      'tracking_parameters' => 'list<KeyValue>',
     );
   }
 }
