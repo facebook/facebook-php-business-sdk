430e25ea9868a71fcb941b045fef3c06205dd22c
Shuotian Cheng <shuotian@meta.com>
2024-09-17 12:24:29 -0700

    Update AdCreativeFeatureDetails Fields
    Summary:
    [BizSDK] Update AdCreativeFeatureDetails Fields
    
    Add the customizations field in AdCreativeFeatureDetails node.
    Also remove some unsupported fields from the AdCreativeFeatureCustomizations node.
    
    Reviewed By: liliarizona
    
    Differential Revision: D62890910
    
    fbshipit-source-id: 1fdad3ce9d8c17811e67dd1d79d5026ce6b27c89

--- src/FacebookAds/Object/Fields/AdCreativeFeatureCustomizationsFields.php
+++ src/FacebookAds/Object/Fields/AdCreativeFeatureCustomizationsFields.php
@@ -22,25 +22,11 @@ use FacebookAds\Enum\AbstractEnum;
 
 class AdCreativeFeatureCustomizationsFields extends AbstractEnum {
 
-  const BACKGROUND_COLOR = 'background_color';
-  const CATALOG_FEED_TAG_NAME = 'catalog_feed_tag_name';
-  const FONT_NAME = 'font_name';
-  const PRODUCT_RECOMMENDATION_TYPE = 'product_recommendation_type';
-  const PRODUCT_VERSIONING_FALLBACK_STRATEGY = 'product_versioning_fallback_strategy';
   const SHOWCASE_CARD_DISPLAY = 'showcase_card_display';
-  const TEXT_STYLE = 'text_style';
-  const VIDEO_CROP_STYLE = 'video_crop_style';
 
   public function getFieldTypes() {
     return array(
-      'background_color' => 'string',
-      'catalog_feed_tag_name' => 'string',
-      'font_name' => 'string',
-      'product_recommendation_type' => 'string',
-      'product_versioning_fallback_strategy' => 'string',
       'showcase_card_display' => 'string',
-      'text_style' => 'string',
-      'video_crop_style' => 'string',
     );
   }
 }
