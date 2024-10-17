3ec36fd90a3847470fae3c5e929640e6cb9ea245
Li Li (AXP) <lil@meta.com>
2024-10-16 17:46:10 -0700

    bump php sdk version by 0.0.1
    Summary: as titled
    
    Reviewed By: stcheng, satwikareddy3
    
    Differential Revision: D64510329
    
    fbshipit-source-id: e006b90dd6328f41849ad63252ce592e684b4558

--- src/FacebookAds/ApiConfig.php
+++ src/FacebookAds/ApiConfig.php
@@ -10,6 +10,6 @@
 namespace FacebookAds;
 class ApiConfig {
   const APIVersion = '21.0';
-  const SDKVersion = '21.0.0';
+  const SDKVersion = '21.0.1';
   const TYPE_CHECKER_STRICT_MODE = false;
 }
